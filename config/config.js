c8dd19fffec8025465eb5464cc4f743f3775adaa
Miad <karoko.jnr99@gmail.com>
2021-06-08 23:43:22 +0300

    env changes

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     MONGO_IP: process.env.MONGO_IP || "mongo",
     MONGO_PORT: process.env.MONGO_PORT || 27017,
-    MONGO_USER: process.env.MONGO_USER,
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+    MONGO_USER: "karokojnr",
+    MONGO_PASSWORD: "karokojnr",
     REDIS_URL: process.env.REDIS_URL || "redis",
     REDIS_PORT: process.REDIS_PORT || 6379,
     SESSION_SECRET: process.env.SESSION_SECRET
